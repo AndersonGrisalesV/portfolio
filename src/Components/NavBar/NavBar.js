3ecc32c9596c133b3d84dcb4242d2f0d2f7f0f23
AndersonGrisalesV <grisalesvargasanderson@gmail.com>
2023-03-14 18:57:44 -0500

    March 14, 2023

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -31,42 +31,50 @@ const NavBar = () => {
 
   return (
     <React.Fragment>
-      {openHamburguerMenu && <Backdrop onClick={closeDrawerHandler} />}
-      <SideDrawer show={openHamburguerMenu} onClick={closeDrawerHandler}>
-        <div className={styles.links__responsive}>
-          <ul className={styles.navbar__links__responsive}>
-            <li className={styles.navbar__item__responsive}>
-              <a href="#home">Home</a>
-            </li>
-            <li className={styles.navbar__item__responsive}>
-              <a href="#expertise">Expertise</a>
-            </li>
-            <li className={styles.navbar__item__responsive}>
-              <a href="#work">Work</a>
-            </li>
-            <li className={styles.navbar__item__responsive}>
-              <a href="#about">About</a>
-            </li>
-            <li className={styles.navbar__item__responsive}>
-              <a href="#contact">Contact</a>
-            </li>
-          </ul>
-          <div className={styles.button__responsive}>
-            <button className={styles.navbar__button__responsive}>Don't</button>
-          </div>
-        </div>
-      </SideDrawer>
-
       <div
         className={`${styles.nav__bar} ${
           visibleNavBar ? "" : styles.nav__bar__hidden
         }`}
         style={{ transitionDelay: visibleNavBar ? "0ms" : "300ms" }}
       >
+        {openHamburguerMenu && <Backdrop onClick={closeDrawerHandler} />}
+        <SideDrawer show={openHamburguerMenu} onClick={closeDrawerHandler}>
+          {/* <div className={styles.menu__responsive}>
+            <HamburguerMenuButton
+              toggled={openHamburguerMenu}
+              toggle={setOpenHamburguerMenu}
+            />
+          </div> */}
+          <div className={styles.links__responsive}>
+            <ul className={styles.navbar__links__responsive}>
+              <li className={styles.navbar__item__responsive}>
+                <a href="#home">Home</a>
+              </li>
+              <li className={styles.navbar__item__responsive}>
+                <a href="#expertise">Expertise</a>
+              </li>
+              <li className={styles.navbar__item__responsive}>
+                <a href="#work">Work</a>
+              </li>
+              <li className={styles.navbar__item__responsive}>
+                <a href="#about">About</a>
+              </li>
+              <li className={styles.navbar__item__responsive}>
+                <a href="#contact">Contact</a>
+              </li>
+            </ul>
+            <div className={styles.button__responsive}>
+              <button className={styles.navbar__button__responsive}>
+                Don't
+              </button>
+            </div>
+          </div>
+        </SideDrawer>
         <div className={styles.links}>
           <div className={styles.logo}>
             <img src={Logo} alt="Logo" />
           </div>
+
           <ul className={styles.navbar__links}>
             <li className={styles.navbar__item}>
               <a href="#home">Home</a>
@@ -84,16 +92,16 @@ const NavBar = () => {
               <a href="#contact">Contact</a>
             </li>
           </ul>
-          <div className={styles.menu}>
-            <HamburguerMenuButton
-              toggled={openHamburguerMenu}
-              toggle={setOpenHamburguerMenu}
-            />
-          </div>
-
           <div className={styles.button}>
             <button className={styles.navbar__button}>Don't</button>
           </div>
+        </div>
+
+        <div className={styles.menu}>
+          <HamburguerMenuButton
+            toggled={openHamburguerMenu}
+            toggle={setOpenHamburguerMenu}
+          />
         </div>
       </div>
     </React.Fragment>
