13f0d35fb4319357398e1a1ae43135794db550cb
AndersonGrisalesV <grisalesvargasanderson@gmail.com>
2023-04-03 18:58:43 -0500

    April 3, 2023

--- src/Components/SectionOne/SectionOne.js
+++ src/Components/SectionOne/SectionOne.js
@@ -6,21 +6,22 @@ import styles from "./SectionOne.module.css";
 const SectionOne = () => {
   return (
     <section className={styles.section__container}>
-      <div className={styles.container__text}>
+      <div className={styles.container__left__section}>
         <div>
-          <h5>Hi, my name is</h5>
+          <h5 className={styles.greeting__text}>Hi, my name is</h5>
         </div>
 
         <div>
-          <h1 className={styles.name}>Anderson Grisales</h1>
+          <h1 className={styles.name__text}>Anderson Grisales</h1>
+        </div>
+
+        <div className={styles.lets__design__container}>
+          <h1 className={styles.lets__text}>Let's</h1>
+          <h1 className={styles.design__text}>design</h1>
         </div>
 
         <div>
-          <h1>Let's design</h1>
-        </div>
-
-        <div>
-          <p className={styles.description}>
+          <p className={styles.description__text}>
             I'm a systems engineer turned web developer and UX/UI designer who
             specializes in crafting exceptional digital experiences. My passion
             for building digital products is driven by a strong emphasis on
@@ -28,7 +29,7 @@ const SectionOne = () => {
           </p>
         </div>
 
-        <div>
+        <div className={styles.button__container}>
           <Button onText={"Resume"}></Button>
         </div>
       </div>
